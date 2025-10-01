9505af2745dc397c994cb09e4b47fe46c8cc4483
Tuneastic <214825290+Tuneastic@users.noreply.github.com>
2025-10-01 13:17:02 +0200

    Add variables to buttons and input in js

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {  
+
+    let screenInput = document.getElementById('input');
+    let btnClear = document.getElementById('clear');
+
+    let btn7 = document.getElementById('7');
+    let btn8 = document.getElementById('8');
+    let btn9 = document.getElementById('9');
+    let btn4 = document.getElementById('4');
+    let btn5 = document.getElementById('5');
+    let btn6 = document.getElementById('6');
+    let btn1 = document.getElementById('1');
+    let btn2 = document.getElementById('2');
+    let btn3 = document.getElementById('3');
+    let btn0 = document.getElementById('0');
+    
+    let btnAdd = document.getElementById('add');
+    let btnSubstract = document.getElementById('substract');
+    let btnMultiply = document.getElementById('multiply');
+    let btnDivide = document.getElementById('divide');
+    let btnEqual = document.getElementById('equal');
+
+
+});
