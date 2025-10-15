1c86b6c8535490a3f6a1b2b2f80f0b89790413eb
Tuneastic <214825290+Tuneastic@users.noreply.github.com>
2025-10-15 15:38:39 +0200

    Edit some variable names for readability

--- script.js
+++ script.js
@@ -13,24 +13,24 @@
         let buttons = [btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0];
         let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
         let keyMap = {
-        '0': document.getElementById('0'),
-        '1': document.getElementById('1'),
-        '2': document.getElementById('2'),
-        '3': document.getElementById('3'),
-        '4': document.getElementById('4'),
-        '5': document.getElementById('5'),
-        '6': document.getElementById('6'),
-        '7': document.getElementById('7'),
-        '8': document.getElementById('8'),
-        '9': document.getElementById('9'),
-        '+': document.getElementById('add'),
-        '-': document.getElementById('substract'),
-        '*': document.getElementById('multiply'),
-        '/': document.getElementById('divide'),
-        '=': document.getElementById('equal'),
-        'Enter': document.getElementById('equal'),
-        'Backspace': document.getElementById('delete'),
-        'Escape': document.getElementById('clear')
+            '0': document.getElementById('0'),
+            '1': document.getElementById('1'),
+            '2': document.getElementById('2'),
+            '3': document.getElementById('3'),
+            '4': document.getElementById('4'),
+            '5': document.getElementById('5'),
+            '6': document.getElementById('6'),
+            '7': document.getElementById('7'),
+            '8': document.getElementById('8'),
+            '9': document.getElementById('9'),
+            '+': document.getElementById('add'),
+            '-': document.getElementById('substract'),
+            '*': document.getElementById('multiply'),
+            '/': document.getElementById('divide'),
+            '=': document.getElementById('equal'),
+            'Enter': document.getElementById('equal'),
+            'Backspace': document.getElementById('delete'),
+            'Escape': document.getElementById('clear')
         };
         let btnClear = document.getElementById('clear');
         let btnDelete = document.getElementById('delete');
@@ -47,10 +47,10 @@
         let operator = '';
         let result;
 
-        document.addEventListener('keydown', event => {
-            let btn = keyMap[event.key];
+        document.addEventListener('keydown', btnPressed => {
+            let btn = keyMap[btnPressed.key];
             if (btn) {
-                event.preventDefault();
+                btnPressed.preventDefault();
                 btn.click();
             }
         });
